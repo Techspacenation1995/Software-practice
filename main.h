d403ec063f34a481d34d8be71bab7f7db675831a
Akinbami Taofeek Olamide <tfkakinbami80@yahoo.com>
2023-05-14 14:52:32 +0000

    Printing the element of an array

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+
 void printReverse(char *s);
 int _putchar (char c);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif
